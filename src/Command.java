a556cde34c021b0e9dcd29ad783a620ecf704dca
Hooi Tong <hooitongx@gmail.com>
2014-09-23 18:18:41 +0800

    Major changes to Command class
    The command class now contains all variables for every type of command.
    Only the required variables will be initialised and become valid for
    usage based on command type.

--- src/Command.java
+++ src/Command.java
@@ -6,23 +6,89 @@
  *
  */
 public class Command {
+    /* Must-have for all types of command */
     private CommandType type;
-    private String[] arguments;
 
-    public Command(CommandType type, String[] args) {
+    /* Information required for add, update */
+    private String description;
+
+    /* Information required for add, update, view */
+    private DatePair dateRange;
+
+    /* Information required for view */
+    private boolean viewAll;
+
+    /* Information required for delete & update */
+    private int taskId;
+
+    /* Information required for search */
+    private String keyword;
+
+    /* Constructor for view command */
+    public Command(CommandType type, boolean viewAll, DatePair dateRange) {
         this.type = type;
-        arguments = args;
+        this.viewAll = viewAll;
+        this.dateRange = dateRange;
     }
 
-    public Command(CommandType type) {
-        this(type, null);
+    /* Constructor for search & invalid command */
+    public Command(CommandType type, String desc) {
+        this.type = type;
+        if (type == CommandType.SEARCH)
+            this.keyword = desc;
+        else if (type == CommandType.INVALID)
+            this.description = desc;
     }
+
+    /* Constructor for add command */
+    public Command(CommandType type, String des, DatePair dateRange) {
+        this.type = type;
+        this.description = des;
+        this.dateRange = dateRange;
+    }
+
+    /* Constructor for delete command */
+    public Command(CommandType type, int taskId) {
+        this.type = type;
+        this.taskId = taskId;
+    }
+
+    /* Constructor for update command */
+    public Command(CommandType type, int taskId, String des, DatePair dateRange) {
+        this.type = type;
+        this.taskId = taskId;
+        this.description = des;
+        this.dateRange = dateRange;
+    }
+
+    /* Constructor for exit command */
+    public Command(CommandType type) {
+        this.type = type;
+    }
+
+    /* Getters methods for variables in the class */
 
     public CommandType getType() {
         return type;
     }
 
-    public String[] getArguments() {
-        return arguments;
+    public String getDescription() {
+        return description;
+    }
+
+    public DatePair getDateRange() {
+        return dateRange;
+    }
+
+    public boolean isViewAll() {
+        return viewAll;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public String getKeyword() {
+        return keyword;
     }
 }
