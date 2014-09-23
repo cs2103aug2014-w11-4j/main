a6e6a93f4800ecb5ca6d2b22cea535d443d9cf3f
Hooi Tong <hooitongx@gmail.com>
2014-09-23 18:19:19 +0800

    Completed parsing for Search, Delete

--- src/Parser.java
+++ src/Parser.java
@@ -6,9 +6,13 @@
  *
  */
 public class Parser {
+    private static final String SEARCH_ERROR_EMPTY = "Please enter a keyword to search for.";
+    private static final String DELETE_ERROR_INVALID = "Please enter a valid id to delete.";
+
     public static Command parse(String input) {
         CommandType userCommand = determineCommandType(input);
-        return parseCommand(userCommand);
+        String args = removeFirstWord(input);
+        return parseCommand(userCommand, args);
     }
 
     private static CommandType determineCommandType(String input) {
@@ -16,32 +20,25 @@ public class Parser {
         return CommandType.getCommandType(command);
     }
 
-    private static Command parseCommand(CommandType userCommand)
-            throws UnsupportedOperationException {
+    private static Command parseCommand(CommandType userCommand, String args) {
         switch (userCommand) {
             case VIEW:
-                throw new UnsupportedOperationException(
-                        "Command not integrated yet");
+                return parseView(args);
 
             case SEARCH:
-                throw new UnsupportedOperationException(
-                        "Command not integrated yet");
+                return parseSearch(args);
 
             case ADD:
-                throw new UnsupportedOperationException(
-                        "Command not integrated yet");
+                return parseAdd(args);
 
             case DELETE:
-                throw new UnsupportedOperationException(
-                        "Command not integrated yet");
+                return parseDelete(args);
 
             case UPDATE:
-                throw new UnsupportedOperationException(
-                        "Command not integrated yet");
+                return parseUpdate(args);
 
             case EXIT:
-                throw new UnsupportedOperationException(
-                        "Command not integrated yet");
+                return parseExit(args);
 
             case INVALID:
                 return new Command(userCommand);
@@ -51,6 +48,45 @@ public class Parser {
                         "Command not integrated yet");
         }
 
+    }
+
+    public static Command parseView(String args) {
+        throw new UnsupportedOperationException("Command not integrated yet");
+    }
+
+    /**
+     * Support parsing for search on basis of keywords.
+     *
+     * @param args user given arguments
+     * @return either invalid or search command
+     */
+    public static Command parseSearch(String args) {
+        if (args.trim().isEmpty()) {
+            return new Command(CommandType.INVALID, SEARCH_ERROR_EMPTY);
+        } else {
+            return new Command(CommandType.SEARCH, args);
+        }
+    }
+
+    public static Command parseAdd(String args) {
+        throw new UnsupportedOperationException("Command not integrated yet");
+    }
+
+    public static Command parseDelete(String args) {
+        try {
+            int deleteId = Integer.parseInt(args.trim());
+            return new Command(CommandType.DELETE, deleteId);
+        } catch (NumberFormatException e) {
+            return new Command(CommandType.INVALID, DELETE_ERROR_INVALID);
+        }
+    }
+
+    public static Command parseUpdate(String args) {
+        throw new UnsupportedOperationException("Command not integrated yet");
+    }
+
+    public static Command parseExit(String args) {
+        return new Command(CommandType.EXIT);
     }
 
     /* Helper Methods for Parser */
