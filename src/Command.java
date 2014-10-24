5200a542ce916275d93f3a3c3b28852d9ba30e2d
ZHAO HANG <davidzhao-81@163.com>
2014-10-24 14:50:48 +0800

    remove clear method

--- src/Command.java
+++ src/Command.java
@@ -200,22 +200,6 @@ public abstract class Command {
         return !(displayedId > displayedTasksList.size() || displayedId <= 0 || displayedTasksList.get(displayedId - 1) == -1);
     }
 
-    /**
-     * Clear the screen of the current interface.
-     *
-     * @throws IOException
-     */
-    protected String clearScreen() throws IOException {
-        final String os = System.getProperty("os.name");
-
-        if (os.contains("Windows")) {
-            Runtime.getRuntime().exec("cls");
-        } else {
-            Runtime.getRuntime().exec("clear");
-        }
-
-        return "";
-    }
 
     /**
      * Method used to check whether a task has any potential conflict in current
