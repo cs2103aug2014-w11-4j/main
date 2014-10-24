1de58340aa360db4f180e902855db51e16945242
ZHAO HANG <davidzhao-81@163.com>
2014-10-24 13:57:18 +0800

    DeleteCommand extends Command

---
+++ src/DeleteCommand.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class DeleteCommand extends Command {
+	private static final String JOURNAL_MESSAGE_DELETE = "Deleted task \"%s\"";
+	private static final String MESSAGE_DELETE = "\"%s\" has been successfully deleted.";
+	private static final String MESSAGE_ERROR_WRONG_TASK_ID = "You have input an invalid ID.";
+
+	public DeleteCommand(CommandType type, int taskId) {
+		this.type = type;
+		this.taskId = taskId;
+	}
+
+	@Override
+	public String execute() throws IOException {
+		if (!isValidDisplayedId(taskId)) {
+			return MESSAGE_ERROR_WRONG_TASK_ID;
+		}
+		long databaseId = displayedTasksList.get(taskId - 1);
+		Task oldTask = dbManager.getInstance(databaseId);
+		String oldDescription = oldTask.getDescription();
+		dbManager.markAsInvalid(databaseId);
+		displayedTasksList.set(taskId - 1, (long) -1);
+		dbManager.recordAction(databaseId, null,
+				String.format(JOURNAL_MESSAGE_DELETE, oldDescription));
+		return String.format(MESSAGE_DELETE, oldDescription);
+
+	}
+}
