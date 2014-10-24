eb0c3b1211dcd6c5b037305a77c39bfc6a3b2dc1
ZHAO HANG <davidzhao-81@163.com>
2014-10-24 14:27:05 +0800

    UndoCommand extends Command

---
+++ src/UndoCommand.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class UndoCommand extends Command {
+	private static final String JOURNAL_MESSAGE_UNDONE = "Undone operation \"%s\".";
+
+	public UndoCommand(CommandType type) {
+		this.type = type;
+	}
+
+	@Override
+	public String execute() throws IOException {
+		try {
+			return String.format(JOURNAL_MESSAGE_UNDONE, dbManager.undo());
+		} catch (UnsupportedOperationException e) { // Nothing to undo
+			return e.getMessage();
+		}
+
+	}
+}
