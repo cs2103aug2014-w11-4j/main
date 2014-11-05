f5f2200ebbc277c82ccf90e5ed97809be083d6c0
Hooi Tong <hooitongx@gmail.com>
2014-11-05 19:16:39 +0800

    Added package-info for Menu

---
+++ src/com/rubberduck/menu/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provide the necessary classes to handle the interface portion of application.
+ * It is responsible for accepting input from the user and passing to the correct
+ * packages. Classes then sends output to interface will also be processed
+ * by classes in this package.
+ */
+package com.rubberduck.menu;
