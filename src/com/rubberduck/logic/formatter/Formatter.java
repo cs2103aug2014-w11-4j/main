485c884940c8f6a77e5e451182a1784bee2dcd1e
Hooi Tong <hooitongx@gmail.com>
2014-11-09 12:26:59 +0800

    Hotfixed Formatter ANSI Offsetting

--- src/com/rubberduck/logic/formatter/Formatter.java
+++ src/com/rubberduck/logic/formatter/Formatter.java
@@ -23,13 +23,15 @@ public class Formatter {
     public static final String FORMAT_TABLE = "%-7s%-6s%-43s%-24s";
     public static final String FORMAT_TENTATIVE = "%-7s%-6s%-43s%-19s%-5s";
 
-    private static final String ANSI_PREFIX = "\u001b[130";
     private static final String SEPARATOR_FLOATING =
         "--------------------------------[    TASKS    ]---------------------------------";
     private static final String SEPARATOR_DEADLINE =
         "--------------------------------[  DEADLINES  ]---------------------------------";
     private static final String SEPARATOR_SCHEDULE =
         "--------------------------------[  SCHEDULES  ]---------------------------------";
+
+    private static final String ANSI_PREFIX = "\u001b[";
+    private static final int ANSI_OFFSET = 7;
 
     /* Values to represent the header type to print */
     /**
@@ -279,7 +281,7 @@ public class Formatter {
         while (!words.isEmpty()) {
             int ansiLength = 0;
             if (words.startsWith(ANSI_PREFIX)) {
-                ansiLength += ANSI_PREFIX.length();
+                ansiLength = ANSI_OFFSET;
             }
             if (words.length() <= maxLength + ansiLength) {
                 wordWrapList.add(words);
