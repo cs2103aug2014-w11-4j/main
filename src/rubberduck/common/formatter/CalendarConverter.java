73ff3959c7274460c225e2355f71f11c5e3ecc64
Hooi Tong <hooitongx@gmail.com>
2014-11-10 09:08:36 +0800

    Added JavaDocs for CalendarConverter

--- src/rubberduck/common/formatter/CalendarConverter.java
+++ src/rubberduck/common/formatter/CalendarConverter.java
@@ -1,11 +1,5 @@
 package rubberduck.common.formatter;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
-
 import com.thoughtworks.xstream.converters.ConversionException;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
@@ -13,25 +7,55 @@ import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-//@author A0119416H
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 /**
- * A simple XStream converter that converts Calendar objects to a human readable format.
+ * A simple XStream converter that converts Calendar objects to a human readable
+ * format.
  */
+//@author A0119416H
 public class CalendarConverter implements Converter {
 
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.US);
+    private static final SimpleDateFormat DATE_FORMAT =
+        new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.US);
 
+    /**
+     * Tells the caller that it can convert anything as long it extends
+     * Calendar.
+     *
+     * @param clazz T Any class object
+     * @return true if it can be converted, else false
+     */
     public boolean canConvert(Class clazz) {
         return Calendar.class.isAssignableFrom(clazz);
     }
 
+    /**
+     * Converts Calendar object into localized String.
+     *
+     * @param value   Calendar object
+     * @param writer  HierarchicalStreamWriter object
+     * @param context MarshallingContext object
+     */
     public void marshal(Object value, HierarchicalStreamWriter writer,
                         MarshallingContext context) {
         Calendar calendar = (Calendar) value;
         writer.setValue(DATE_FORMAT.format(calendar.getTime()));
     }
 
+    /**
+     * Retrieves and converts the localized DateFormat instance and parses the
+     * String into a Date and puts this date into original GregorianCalendar
+     * object
+     *
+     * @param reader  HierarchicalStreamReader object
+     * @param context UnmarshallingContext object
+     * @return Calendar object
+     */
     public Object unmarshal(HierarchicalStreamReader reader,
                             UnmarshallingContext context) {
         GregorianCalendar calendar = new GregorianCalendar();
@@ -42,5 +66,4 @@ public class CalendarConverter implements Converter {
         }
         return calendar;
     }
-
 }
